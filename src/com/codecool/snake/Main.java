a57204c2dce8f3d37f1c755dd708e9e52925a187
Selmeczi Tamas <dizsit@gmail.com>
2017-09-06 15:26:37 +0200

    fixing import

--- src/com/codecool/snake/Main.java
+++ src/com/codecool/snake/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Random;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
