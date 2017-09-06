8bf80a9e7b291277f12f1f360bc481b31401e32b
Selmeczi Tamas <dizsit@gmail.com>
2017-09-06 12:59:08 +0200

    Add randInt generator to the Main class

--- src/com/codecool/snake/Main.java
+++ src/com/codecool/snake/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Random;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -18,6 +20,12 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(game, Globals.WINDOW_WIDTH, Globals.WINDOW_HEIGHT));
         primaryStage.show();
         game.start();
+    }
+
+    public static int randInt(int min, int max) {
+        Random random = new Random();
+        int randomNumber = random.nextInt((max - min) + 1) + min;
+        return randomNumber;
     }
 
 }
